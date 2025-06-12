18bdf865f881af26c49e53bce66a010b39335dbb
cake <cakeistastydev@gmail.com>
2025-06-12 21:39:34 +0100

    Fix to creative tab big oopsie daisy

--- src/main/java/com/cake/drill_drain/mixin/CreateCreativeModeTabMixin.java
+++ src/main/java/com/cake/drill_drain/mixin/CreateCreativeModeTabMixin.java
@@ -16,16 +16,16 @@ import java.util.function.Function;
 @Mixin(remap = false, targets = "com.simibubi.create.AllCreativeModeTabs$RegistrateDisplayItemsGenerator")
 public class CreateCreativeModeTabMixin {
 
-    @Inject(method = "makeVisibilityFunc", order = 400, at = @At(value = "RETURN"), cancellable = true)
-    private static void outputAll(CallbackInfoReturnable<Function<Item, CreativeModeTab.TabVisibility>> cir) {
-        Function<Item, CreativeModeTab.TabVisibility> wrappedFunc = cir.getReturnValue();
-        cir.setReturnValue((item) -> {
-            if (!item.builtInRegistryHolder().key().location().getPath().equals("create")) {
-                return CreativeModeTab.TabVisibility.PARENT_TAB_ONLY;
-            }
-            return wrappedFunc.apply(item);
-        });
-    }
+//    @Inject(method = "makeVisibilityFunc", order = 400, at = @At(value = "RETURN"), cancellable = true)
+//    private static void outputAll(CallbackInfoReturnable<Function<Item, CreativeModeTab.TabVisibility>> cir) {
+//        Function<Item, CreativeModeTab.TabVisibility> wrappedFunc = cir.getReturnValue();
+//        cir.setReturnValue((item) -> {
+////            if (!item.builtInRegistryHolder().key().location().getPath().equals("create")) {
+////                return CreativeModeTab.TabVisibility.PARENT_TAB_ONLY;
+////            }
+//            return wrappedFunc.apply(item);
+//        });
+//    }
 
     @Inject(method = "applyOrderings", order = 400, at = @At(value = "TAIL"))
     private static void outputAll(List<Item> items, List<?> orderings, CallbackInfo ci) {
