a18ab8375fd02031662c38374de7659ec1bbc3e6
cake <cakeistastydev@gmail.com>
2025-06-12 18:53:32 +0100

    compat fix

--- src/main/java/com/cake/drill_drain/mixin/CreateCreativeModeTabMixin.java
+++ src/main/java/com/cake/drill_drain/mixin/CreateCreativeModeTabMixin.java
@@ -1,6 +1,7 @@
 package com.cake.drill_drain.mixin;
 
 import com.cake.drill_drain.foundation.DDTabInsertions;
+import com.simibubi.create.Create;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,18 +16,18 @@ import java.util.function.Function;
 @Mixin(remap = false, targets = "com.simibubi.create.AllCreativeModeTabs$RegistrateDisplayItemsGenerator")
 public class CreateCreativeModeTabMixin {
 
-    @Inject(method = "makeVisibilityFunc", order = 600, at = @At(value = "RETURN"), cancellable = true)
+    @Inject(method = "makeVisibilityFunc", order = 400, at = @At(value = "RETURN"), cancellable = true)
     private static void outputAll(CallbackInfoReturnable<Function<Item, CreativeModeTab.TabVisibility>> cir) {
         Function<Item, CreativeModeTab.TabVisibility> wrappedFunc = cir.getReturnValue();
         cir.setReturnValue((item) -> {
-            if (DDTabInsertions.getAllInsertsAfter().values().contains(item)) {
+            if (!item.builtInRegistryHolder().key().location().getPath().equals("create")) {
                 return CreativeModeTab.TabVisibility.PARENT_TAB_ONLY;
             }
             return wrappedFunc.apply(item);
         });
     }
 
-    @Inject(method = "applyOrderings", order = 600, at = @At(value = "TAIL"))
+    @Inject(method = "applyOrderings", order = 400, at = @At(value = "TAIL"))
     private static void outputAll(List<Item> items, List<?> orderings, CallbackInfo ci) {
         for (int i = 0; i < items.size(); i++) {
             if (DDTabInsertions.getAllInsertsAfter().containsKey(items.get(i))) {
